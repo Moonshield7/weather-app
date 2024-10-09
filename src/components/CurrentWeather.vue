5808e5092b9d1dd8296d21b1405e3b5332d75e4e
Moonshield7 <fannie.p@hotmail.fr>
2024-10-09 15:20:36 +0200

    Fixing the select

--- src/components/CurrentWeather.vue
+++ src/components/CurrentWeather.vue
@@ -7,12 +7,12 @@ let mockData = {
   temperature: 666,
   initialized: moment().format('LL')
 }
-
+let selectedCity = ref('Rennes')
 let options = ref([
-  { value: 'Rennes' },
-  { value: 'Tchikiland' },
-  { value: 'Truc' },
-  { value: 'Bidule' }
+  { text: 'Rennes', value: 'Rennes' },
+  { text: 'Tchikiland', value: 'Tchikiland' },
+  { text: 'Truc', value: 'Truc' },
+  { text: 'Bidule', value: 'Bidule' }
 ])
 </script>
 
@@ -20,9 +20,11 @@ let options = ref([
   <div class="main-container">
     <h2>Où ?</h2>
     <select v-model="selectedCity" name="city" id="select-city">
-      <option v-for="option in options" :value="option.value">{{ option.value }}</option>
+      <option v-for="option in options" :value="option.value" :key="option.value">
+        {{ option.text }}
+      </option>
     </select>
-    <buttonc class="search-button">Search</buttonc>
+    <button class="search-button">Search</button>
   </div>
   <div class="main-container">
     <h2>Météo Actuelle à {{ selectedCity }}</h2>
@@ -53,11 +55,13 @@ let options = ref([
   margin-top: 10px;
   background-color: rgb(64, 64, 244);
   border-radius: 5px;
+  border: none;
   color: white;
-  padding: 5px;
+  /* padding: 5px; */
   text-align: center;
   width: 70px;
-  height: 20px;
+  height: 25px;
+  cursor: pointer;
 }
 
 /* h1 {
